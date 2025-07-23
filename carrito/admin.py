9f701b4091a9a4765c59aefe213b66cda6a54c1e
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2025-07-23 14:17:41 -0500

    creacion en admin de los modelos para visualizar en el modulo superuser

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Producto, Carrito, ItemCarrito
+
+admin.site.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'cantidad')
+    search_fields = ('nombre',)
+    
+admin.site.register(Carrito)    
+class CarritoAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+    search_fields = ('id',)
+    
+    
+admin.site.register(ItemCarrito)
+class ItemCarritoAdmin(admin.ModelAdmin):
+    list_display = ('carrito', 'producto', 'cantidad')
+    search_fields = ('carrito__id', 'producto__nombre') 
--- carrito/models.py
+++ carrito/models.py
@@ -6,7 +6,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    cantidad = models.PositiveIntegerField()
     
     def __str__(self):
         return self.nombre
